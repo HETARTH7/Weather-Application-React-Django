0ba32c4be68841987ff27636cc098afb5dbc4232
unknown <hetarth002@gmail.com>
2023-09-24 17:47:35 +0530

    add responsive behavior and modify server requirements

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Weather Application
+# Usage
+
+## Frontend
+
+```
+cd client
+```
+
+Install dependencies
+
+```
+npm i
+```
+
+Start React app
+
+```
+npm start
+```
+
+## Backend
+
+```
+cd server
+```
+
+Install dependencies
+
+```
+pip install -r requirements.txt
+```
+
+Run the server
+
+```
+cd
+python .\manage.py runserver
+```
--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 const App = () => {
   const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const onChangeCity = (e) => {
@@ -12,20 +13,58 @@ const App = () => {
 
   const fetchData = async (e) => {
     e.preventDefault();
+    setLoading(true);
+
     try {
       const response = await axios.get(`http://localhost:8000/?city=${city}`);
-      console.log(response);
+      const data = response.data.current;
+      setWeatherData({
+        conditions: data.condition.text,
+        temperature: data.temp_c,
+        humidity: data.humidity,
+      });
     } catch (err) {
       console.log(err);
+    } finally {
+      setLoading(false);
     }
   };
 
   return (
-    <div>
-      <form onSubmit={fetchData}>
-        <input onChange={onChangeCity} />
-        <input type="submit" />
-      </form>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h1 className="text-center mb-4">Weather App</h1>
+          <form onSubmit={fetchData}>
+            <div className="input-group mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Enter city name"
+                onChange={onChangeCity}
+              />
+              <div className="input-group-append">
+                <button className="btn btn-primary" type="submit">
+                  Get Weather
+                </button>
+              </div>
+            </div>
+          </form>
+
+          {loading ? (
+            <p className="text-center">Loading...</p>
+          ) : (
+            weatherData && (
+              <div className="text-center">
+                <h2>Weather for {city}</h2>
+                <p>Conditions: {weatherData.conditions}</p>
+                <p>Temperature: {weatherData.temperature}°C</p>
+                <p>Humidity: {weatherData.humidity}%</p>
+              </div>
+            )
+          )}
+        </div>
+      </div>
     </div>
   );
 };
